caaa5e66be04a7de47b0cd403c9d0dd77a926339
Ludchenko <skillet367@icloud.com>
2020-03-26 00:01:37 +0200

    3) Added method Say()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
     }
+
+    public static void say() {
+        System.out.println("Hello!");
+    }
 }
